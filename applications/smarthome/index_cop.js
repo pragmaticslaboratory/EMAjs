3b1e1ea6bc820a46350998af241421e98b636479
Pau Leger <pleger@gmail.com>
2021-11-24 12:11:48 -0500

    - Minor fixes - Rename test file names - adding few tests to check parameters used in partial methods and proceed.

---
+++ applications/smarthome/index_cop.js
@@ -0,0 +1,7 @@
+const Home = require("./home");
+
+let home = new Home();
+let bedroom = home.rooms[1];
+let tv = bedroom.getAppliance("tv");
+
+//MAGIC TO bE DEFINED
--- tests/9_testPatialMethodLayer.js
+++ tests/9_testPatialMethodLayer.js
@@ -257,5 +257,64 @@ module.exports = testCase({
         test.deepEqual(flags, ["original", "variation-original", "original"]);
 
         test.done();
+    },
+    'layer-method-with-args_1': function (test) {
+        let flags = [];
+        let obj = {
+            x: new Signal(-1),
+            m: function (x) {
+                flags.push("original_" + x);
+            },
+        };
+
+        let layer = {
+            condition: new SignalComp("a > 1")
+        };
+
+        obj.x.value = 10;
+        EMA.exhibit(obj, {a: obj.x});
+        EMA.addPartialMethod(layer, obj, "m", function () {
+            let arg = arguments[0];
+            flags.push("variation_" + arg);
+        });
+
+        obj.m(1);
+        EMA.deploy(layer);
+        obj.m(2);
+        obj.x.value = 0;
+        obj.m(3);
+        test.deepEqual(flags, ["original_1", "variation_2", "original_3"]);
+
+        test.done();
+    },
+    'layer-method-with-args_proceed': function (test) {
+        let flags = [];
+        let obj = {
+            x: new Signal(-1),
+            m: function (x) {
+                flags.push("original_" + x);
+            },
+        };
+
+        let layer = {
+            condition: new SignalComp("a > 1")
+        };
+
+        obj.x.value = 10;
+        EMA.exhibit(obj, {a: obj.x});
+        EMA.addPartialMethod(layer, obj, "m", function () {
+            let arg = arguments[0];
+            flags.push("variation_" + arg);
+            Layer.proceed(arg + 1);
+        });
+
+        obj.m(1);
+        EMA.deploy(layer);
+        obj.m(2);
+        obj.x.value = 0;
+        obj.m(3);
+        test.deepEqual(flags, ["original_1", "variation_2", "original_3", "original_3"]);
+
+        test.done();
     }
 });
