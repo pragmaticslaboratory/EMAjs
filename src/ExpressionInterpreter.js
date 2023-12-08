094897b1605027ae79f99f802cb53ffe038362b9
danielzazzali <dazazzali@gmail.com>
2023-12-08 14:38:45 -0300

    Refactor expression evaluation to remove 'with' usage for enhanced safety

--- src/ExpressionInterpreter.js
+++ src/ExpressionInterpreter.js
@@ -3,9 +3,17 @@ function expressionInterpreter(expression, contextObj) {
     let result;
 
     try {
-        with (contextObj) {
-            result = eval(expression);
-        }
+        // Dynamically create a new function using the keys of contextObj as parameters
+        // and the provided expression as the function body. This function, when called,
+        // will evaluate the expression in the context of the values provided in contextObj.
+        // For example, if contextObj = { x: 1, y: 2 } and expression = "x + y",
+        // this creates a function equivalent to: function(x, y) { return x + y; }
+        const func = new Function(...Object.keys(contextObj), `return ${expression};`);
+
+        // Execute the dynamically created function, passing in the values from contextObj.
+        // This effectively evaluates the expression using the values from contextObj.
+        // Continuing the previous example, this is akin to calling func(1, 2), which returns 3.
+        result = func(...Object.values(contextObj));
     } catch (error) {
         if (error instanceof ReferenceError) {
             return false; //return false when it is not possible to evaluate
